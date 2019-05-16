c66bc44f60a99dcb6b37414cdefc0f5e5b820bc4
Tomasz Wojtkowiak <tomasz.wojtkowiak@peritum.net>
2019-05-16 21:34:51 +0200

    Initial commit

---
+++ M.O.C/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  M.O.C
+//
+//  Created by Tomasz Wojtkowiak on 16/05/2019.
+//  Copyright © 2019 Peritum.Net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

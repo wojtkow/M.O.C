72bde17ed3b3023031706e67af7449cccb93ae7c
Tomasz Wojtkowiak <tomasz.wojtkowiak@peritum.net>
2019-05-18 18:38:51 +0200

    Added first two endpoints to OpenCachingRouter

--- M.O.C/API/OpenCachingRouter.swift
+++ M.O.C/API/OpenCachingRouter.swift
@@ -12,4 +12,41 @@ import Alamofire
 enum OpenCachingRouter: URLRequestConvertible {
 	static let baseURL = "https://opencaching.pl/okapi/"
 
+	case getCeocache([String: Any])
+	case getNearestGeochaches([String: Any])
+	
+	func asURLRequest() throws -> URLRequest {
+		var method: HTTPMethod {
+			return .get
+		}
+		
+		
+		let url: URL = {
+			let relativePath: String
+			switch self {
+			case .getCeocache:
+				relativePath = "services/caches/geocaches"
+			case .getNearestGeochaches:
+				relativePath = "services/caches/search/nearest"
+			}
+			
+			var url = URL(string: OpenCachingRouter.baseURL)!
+			url.appendPathComponent(relativePath)
+			return url
+		}()
+		
+		let params: ([String: Any]?) = {
+			switch self {
+			case .getCeocache(let params), .getNearestGeochaches(let params):
+				return params
+			}
+		}()
+		
+		var baseRequest = URLRequest(url: url)
+		baseRequest.httpMethod = method.rawValue
+		
+		let encoding = JSONEncoding.default
+		let urlRequest = try encoding.encode(baseRequest, with: params)
+		return urlRequest
+	}
 }
